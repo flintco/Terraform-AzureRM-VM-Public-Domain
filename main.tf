980e35ad36000111940d6b214504aa26c55a36cf
Connor Flint <48108716+ConnorF99@users.noreply.github.com>
2020-12-14 18:09:08 -0600

    First commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  version = "=2.40.0"
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "North Central US"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
