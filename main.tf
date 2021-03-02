fab9d18c2f6470af5df1ce0cd6d94b213d36e7f9
flintco <connor.flint16@gmail.com>
2021-03-02 14:47:01 -0600

    Created Public IP Resource

--- main.tf
+++ main.tf
@@ -6,22 +6,31 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "Resource Group"
+  name     = "ResourceGroup"
   location = "North Central US"
 }
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "vnet" {
-  name                = "Virtual Network"
+  name                = "VirtualNetwork"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
 }
 
-#Subnet comment
+#Create a subnet within the virtual network
 resource "azurerm_subnet" "subnet"{
   name = "Subnet"
   resource_group_name = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes = [ "10.0.1.0/24" ]
 }
+
+#Create a public IP resource
+resource "azurerm_public_ip" "pip"{
+  name = "PublicIP"
+  location = "eastus"
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method = "Dynamic"
+}
+
