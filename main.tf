be73253cda79b627b7973f2471c777d36b5ed42f
flintco <connor.flint16@gmail.com>
2021-03-09 16:41:07 -0600

    Added Security rule to allow HTTP access

--- main.tf
+++ main.tf
@@ -40,6 +40,7 @@ resource "azurerm_network_security_group" "nsg"{
   location = "eastus"
   resource_group_name = azurerm_resource_group.rg.name
 
+  #SSH Access
   security_rule = {
     access = "Allow"
     destination_address_prefix = "*"
@@ -51,9 +52,18 @@ resource "azurerm_network_security_group" "nsg"{
     source_address_prefix = "*"
     source_port_range = "*"
   }
-
-  #Add Security rule for HTTP access
-
+  #HTTP access
+  security_rule {
+        name                       = "HTTP"
+        priority                   = 101
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
 }
 
 #Create a Network Interface Card to connect VM, Public IP, and Vnet
