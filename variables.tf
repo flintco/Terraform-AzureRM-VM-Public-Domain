0143b16baf078f4d0c2a6e21631039544f3c1fda
flintco <connor.flint16@gmail.com>
2021-03-10 12:53:34 -0600

    Created variables.tf file

---
+++ variables.tf
@@ -0,0 +1,11 @@
+#Application Abbrevation (this will be used as a prefix for your resources)
+
+#Domain Name
+
+#Azure Region (Region where your resources will be physically located)
+#Default eastus
+
+#VM Admin Username. You will use this when you SSH into VM.
+#Default should be adminuser
+
+#VM Size
