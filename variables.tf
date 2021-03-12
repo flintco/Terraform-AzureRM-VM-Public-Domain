9539a222cc4c6c5bedb8a0ebb8147fc22a6d35bd
flintco <connor.flint16@gmail.com>
2021-03-12 11:09:59 -0600

    Created all variables needed

--- variables.tf
+++ variables.tf
@@ -5,18 +5,27 @@ variable "abbreviation" {
 }
 
 #Domain Name
+variable "domain" {
+  type = string
+  default = "mysite.com"
+}
 
 #Azure Region (Region where your resources will be physically located)
-#Default eastus
+variable "location" {
+  type = string
+  default = "eastus"
+}
 
 #VM Admin Username. You will use this when you SSH into VM.
-#Default should be adminuser
+variable "admin" {
+  type = string
+  default = "adminuser"
+}
 
 #VM Size
+variable "vmsize" {
+  type = string
+  default = "Standard_DS1_v2"
+}
 
-
-/*For all:
-type = string
-default = "";
-
-To access in terrafrom var.DomainName
+//To access in terrafrom var.DomainName
